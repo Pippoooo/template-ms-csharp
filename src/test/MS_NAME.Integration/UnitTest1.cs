aa648adfc9bbe667e884ab8af71b4de324fe576d
Filippo Marcantonio <filippomarcantonio005@gmail.com>
2025-11-23 11:00:42 +0100

    solution and template projects

---
+++ src/test/MS_NAME.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MS_NAME.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ src/test/MS_NAME.Unit/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace MS_NAME.Unit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
